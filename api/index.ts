e124681c7ac2010d5fe19189b5dc1c2872a79390
yasirarism <yasiramunandar@gmail.com>
2023-02-15 03:09:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.ts
@@ -0,0 +1,25 @@
+import { getScreenshot } from "./_lib/puppeteer";
+
+module.exports = async (req, res) => {
+  if (!req.query.url) return res.status(400).send("No url query specified.");
+  if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
+  try {
+    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
+    res.status(200).end(file);
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
+  }
+}
+
+function checkUrl(string, hostname) {
+  var url = "";
+  try {
+    url = new URL(string);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
